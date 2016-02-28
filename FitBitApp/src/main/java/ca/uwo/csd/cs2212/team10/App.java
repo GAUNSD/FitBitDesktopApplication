399bcc7517f0f8ff75a5dbe2f30b9c6c1231e673
Neeraja Murali <Neeraja Murali>
2016-02-28 12:57:38 -0500

    Updated the UI

--- FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/App.java
+++ FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/App.java
@@ -1,14 +1,23 @@
-package ca.uwo.csd.cs2212.team10;
-
 import javax.swing.SwingUtilities;
+
+import org.json.JSONException;
 
 public class App {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
-			
+
 			public void run() {
-				MainWindow window =  new MainWindow();
+				MainWindow window = null;
+				try {
+					window = new MainWindow();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (TokensException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				window.setVisible(true);
 			}
 		});
--- FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/MainWindow.java
+++ FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/MainWindow.java
@@ -1,21 +1,27 @@
-package ca.uwo.csd.cs2212.team10;
-
 import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
 
+import org.json.JSONException;
+/**
+ * A class that creates the main UI windows 
+ * It calls out the MainTabWindow that has all the panels 
+ * CS 2212 FitBit
+ * @author Team 10
+ *
+ */
 public class MainWindow extends JFrame{
 	
 	
-	public MainWindow() {
+	public MainWindow() throws JSONException, TokensException {
 		this.initUI();
 	}
 
-	private void initUI() {
+	private void initUI() throws JSONException, TokensException {
 		// Create and set up the window with its initial attributes.
 		this.setTitle("Fit Bit");
-		this.setSize(1200, 675); // The screen size should be 16x9; We use a scale factor of 75
+		this.setSize(1230, 685); // The screen size should be 16x9; We use a scale factor of 75
 	    this.setLocationRelativeTo(null);
 	    this.setBackground(new Color(55, 55, 55));
 	    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -25,49 +31,25 @@ public class MainWindow extends JFrame{
 	  	this.setJMenuBar(this.createMenubar());
 	    
 	    // Create the Main Tab Window. This JPanel will be used to navigate through the window
- 		MainTabbedWindow mainTabWindow = new MainTabbedWindow();
-		this.getContentPane().add(mainTabWindow, BorderLayout.CENTER);
-	
-		//this.pack()	<-- Is this needed? Investigate
-	}
+ 		MainTabWindow mainTabWindow = new MainTabWindow();
+		getContentPane().add(mainTabWindow, BorderLayout.CENTER);
+			}
 
 	private JMenuBar createMenubar() {
 		// Create the Top Menu Bar and set its attributes
-		JMenuBar topMenubar = new JMenuBar();
-		topMenubar.setOpaque(true);
+		MyMenuBar topMenubar = new MyMenuBar();
 		topMenubar.setBackground(new Color(87, 87, 87));
-		topMenubar.setPreferredSize(new Dimension(1200, 70)); // height 50. higher? or lower?
-		
-		// Create the 'User Name' menu item. Here the user will be able to exit or logout (expand options in the future)
-		JMenu mnuUserName = new JMenu("User Name");
-		mnuUserName.setForeground(Color.WHITE);
-		mnuUserName.setBackground(Color.WHITE);
-		mnuUserName.setMnemonic(KeyEvent.VK_F);
-		// Under 'User Name': Logout
-		//	this lets the user logout. Takes them to the Sign in page
-		JMenuItem mniUserNameLogout = new JMenuItem("Log Out");
-		//..
-		//	Implement this option taking them to the Sign In in page
-		//		Do this by learning how the addActionListener works
-		// 		This is the key to adding all the actions
-		//..
-		
-		// Also under 'User Name': Exit
-		//	This lets the user exit the program entirely. (implement the exiting also singing them out and saving settings)
-		JMenuItem mniUserNameExit = new JMenuItem("Exit");
-		mniUserNameExit.setMnemonic(KeyEvent.VK_E);
-		mniUserNameExit.setToolTipText("Exit application");
-		mniUserNameExit.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				System.exit(0); }
-		});
+		topMenubar.setPreferredSize(new Dimension(1200, 70)); // height
 		
 		// Add all the created elements to their respective containers
-		mnuUserName.add(mniUserNameLogout);
-		mnuUserName.add(mniUserNameExit);
 		topMenubar.add(Box.createHorizontalGlue()); // This spaces out the menu item so it's to the left
-		topMenubar.add(mnuUserName);
+		JLabel lblNewLabel = new JLabel("");
+		ImageIcon img0=new ImageIcon("src/main/resources/fitbitlogo.png");
+		lblNewLabel.setIcon(img0);
+		topMenubar.add(lblNewLabel);
+		topMenubar.add(Box.createHorizontalGlue()); // This spaces out the menu item so it's to the left
+		
+		
 		return topMenubar;	
 	}
 }
