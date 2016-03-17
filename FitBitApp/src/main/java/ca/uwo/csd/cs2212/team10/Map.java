abb9821510b32cc7720a99f65ab20e58b6d1e895
Conor Dunne <cdunne8@uwo.ca>
2016-03-17 01:40:41 -0400

    Updated Map.java.

--- FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/Map.java
+++ FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/Map.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.StringBuffer; 
 import java.net.URL;
 import java.util.Iterator;
 
@@ -50,15 +52,15 @@ public class Map {
 
         // Read from locations.json
         /*
-        JSONParser parser = new JSONParser();
+           JSONParser parser = new JSONParser();
 
-        Object obj = parser.parse(new FileReader("src/main/resources/locationBank.json"));
-        JSONObject object = (JSONObject) obj; 
+           Object obj = parser.parse(new FileReader("src/main/resources/locationBank.json"));
+           JSONObject object = (JSONObject) obj; 
 
-        JSONArray locations = object.getJSONArray("locations"); 
+           JSONArray locations = object.getJSONArray("locations"); 
 
-        for (int i = 0; i < locations.length(); i++) {
-        }*/
+           for (int i = 0; i < locations.length(); i++) {
+           }*/
     }
 
     private double getLifeDistance() {
@@ -70,23 +72,57 @@ public class Map {
         return 0;
     }
 
+    private String readURL(String requestURL) throws Exception {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(requestURL);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read); 
+
+            return buffer.toString();
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
+    }
+    
     public Location getNewLocation(String locationName) {
         // Call google API to get coordinates, and city name
-        // return location object 
-        return new Location("","");
+        // return location object
+        String requestURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locationName.replaceAll("\\s","%20") + "&key=AIzaSyA3qYxpHJKnTbHfW1oRcCSpycKqKUvwvV0";
+        try {
+            JSONObject object = new JSONObject(readURL(requestURL));
+            
+            JSONObject results = object.getJSONObject("results");
+            String formattedAddress = results.getString("formatted_address");
+            
+            JSONObject geometry = results.getJSONObject("geometry");
+            JSONObject locationCoordinates = geometry.getJSONObject("location");
+            String coords = locationCoordinates.getString("lat") + locationCoordinates.getString("lng");
+
+        } catch(JSONException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return new Location(formattedAddress,coords); 
     }
 
-    public void setCurrentLocation(Location currentLocation) {
-        this.currentLocation = currentLocation;
-        // Calculate distances, write to locations.json, write image of city src/main/resources/locationImages/
-    }
+public void setCurrentLocation(Location currentLocation) {
+    this.currentLocation = currentLocation;
+    // Calculate distances, write to locations.json, write image of city src/main/resources/locationImages/
+}
 
 
-    public boolean getLocationSet() {
-        return this.locationSet; 
-    }
-    public Location getCurrentLocation() {
-        return this.currentLocation;
-    }
+public boolean getLocationSet() {
+    return this.locationSet; 
+}
+public Location getCurrentLocation() {
+    return this.currentLocation;
+}
 
 }
