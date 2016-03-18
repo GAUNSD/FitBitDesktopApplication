1344d344297e0eb58e3ef3c430f06f5289ea286f
Gustavo Murcia <gmurcia@uwo.ca>
2016-03-18 02:35:45 -0400

    Added small test harnesses for ActiveMinute and Map, this is for testing the drawing of the graphics for each respective class. Some of this may not be implemented, but could be useful when drawing other classes

--- FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/ActiveMinutesFrame.java
+++ FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/ActiveMinutesFrame.java
@@ -3,6 +3,7 @@ package ca.uwo.csd.cs2212.team10;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 /**
  * This class represents the information for the Dashboard element 'Active Minutes'
@@ -12,7 +13,12 @@ import java.awt.*;
  * @author Gustavo Murcia
  */
 public class ActiveMinutesFrame extends JPanel {
-
+	//TMP
+	private final static int MAX_PROGRESS_AMOUNT = 100;
+	private static final int DELAY = 50;
+	private Timer timer;
+	private int prgValue = 0;
+	//
 	/**
 	 * The constructor for Active Minutes Frame. 
 	 * The parameters represent information coming from the API
@@ -25,7 +31,7 @@ public class ActiveMinutesFrame extends JPanel {
 	public ActiveMinutesFrame(int lightActiveMins, int fairlyActiveMins, int veryActiveMins, int activeMinGoals) {
 		// Change GridLayout to better organize the panel
 		super(new GridLayout(1, 1));
-		
+
 		// JLabels to print the text for the testFitBitAPI
 		String stringLabel = "<html>" +
 				"<u>Active Minutes</u>" +
@@ -35,13 +41,84 @@ public class ActiveMinutesFrame extends JPanel {
 				"<br><br>Goals: " + activeMinGoals + 
 				"</html>";
 		JLabel lblName = new JLabel(stringLabel, JLabel.CENTER);
-		
+
 		lblName.setOpaque(false);	
 		lblName.setToolTipText("tmp");
 		this.setBackground(new Color(150, 150, 150));
-		
-		
+
+		//Calculate the progress of the Active Minute Goals	
+		double d = (lightActiveMins + fairlyActiveMins + veryActiveMins)/(double)activeMinGoals;
+		int angleProgress = (int)((d)*360);
+
+		System.out.println(angleProgress);
+		/////This might not be the best way to do it
+		ProgressBar progBar = new ProgressBar();
+		progBar.setArcAngle(angleProgress);
 		//this.add(content);
+		this.add(progBar);
 		this.add(lblName);
 	}
+
+	/*
+	 * Private class to draw the progress bar for the active minutes
+	 */
+	private class ProgressBar extends JComponent {
+		Ellipse2D.Double fill, inside;
+		int x = 10;
+		int y = 10;
+		int width = 100;
+		int height = 100;
+		int startAngle = 0; 
+		int arcAngle = 270;
+		
+		public void setArcAngle(int angle) {
+			this.arcAngle = angle;
+		}
+		
+		@Override
+		public void paint(Graphics g) {
+			super.paintComponent(g);
+			Graphics2D g2 = (Graphics2D) g;
+
+			g.setColor(Color.RED);
+
+
+			g.fillArc(10, 10, 100, 100, 0, arcAngle);
+			//g.fillArc(15, 15, 85, 85, 0, 360);
+
+
+		}
+	}
+	public static void main(String[] args) {
+		// Test Data 
+		int lightActiveMins = 1234;
+		int fairlyActiveMins = 456;
+		int veryActiveMins = 789;
+		int activeMinGoals = 1500;
+
+		// Create the Frames needed 
+		ActiveMinutesFrame testActiveMin = new ActiveMinutesFrame(lightActiveMins, fairlyActiveMins,veryActiveMins,activeMinGoals);
+		JInternalFrame testFrame = new JInternalFrame("Test");
+		testFrame.setLocation(100, 100 );
+		testFrame.setSize( 200, 200 );
+		testFrame.setVisible( true );
+		testFrame.setResizable( false );
+		testFrame.setIconifiable( false );
+		JDesktopPane testPanel = new JDesktopPane();
+		JFrame testOFrame = new JFrame();
+		testOFrame.setSize(1000, 800);
+
+		// add the elements for the frame
+
+		testFrame.setSize(600, 400);
+		testFrame.getContentPane().add(testActiveMin, BorderLayout.CENTER);
+
+		testPanel.add(testFrame);
+
+		testOFrame.add(testPanel);
+		testOFrame.setVisible(true);
+	}
 }
+
+
+
--- FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/MapFrame.java
+++ FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/MapFrame.java
@@ -42,4 +42,34 @@ public class MapFrame extends JPanel {
 		//this.add(content);
 		this.add(lblName);
 	}
+	
+	// TMP test
+	public static void main(String[] args) {
+		// Test Data 
+		double bestDistance = 123.6;
+		String bestDistanceDate = "2016.02.03";
+		double lifeDistance = 321.6;
+		
+		// Create the Frames needed 
+		MapFrame testMap = new MapFrame(bestDistance, bestDistanceDate, lifeDistance);
+		JInternalFrame testFrame = new JInternalFrame("Test");
+		testFrame.setLocation(100, 100 );
+		testFrame.setSize( 200, 200 );
+		testFrame.setVisible( true );
+		testFrame.setResizable( false );
+		testFrame.setIconifiable( false );
+		JDesktopPane testPanel = new JDesktopPane();
+		JFrame testOFrame = new JFrame();
+		testOFrame.setSize(1000, 800);
+		
+		// add the elements for the frame
+		
+		testFrame.setSize(600, 400);
+		testFrame.getContentPane().add(testMap, BorderLayout.CENTER);
+		
+		testPanel.add(testFrame);
+		
+		testOFrame.add(testPanel);
+		testOFrame.setVisible(true);
+	}
 }
