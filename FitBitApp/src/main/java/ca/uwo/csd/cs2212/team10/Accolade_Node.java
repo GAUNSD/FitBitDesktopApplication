27ef7633a1d5f334c27757ac0f0b214e955b4971
Neeraja Murali <Neeraja Murali>
2016-03-23 11:20:07 -0400

    Added comments to all classes and updated badge sizes

--- FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/Accolade_Node.java
+++ FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/Accolade_Node.java
@@ -1,36 +1,84 @@
 package ca.uwo.csd.cs2212.team10;
 
+/**
+ * This class represents one accolade 
+ * @author Neeraja Murali Dharan 
+ */
+
 public class Accolade_Node {
+	//Fields representing information for an accolade 
 	private String description; 
 	private String title;
 	private boolean check;
 	
+	/**
+	 * Accolades constructor 
+	 * intializing all values to null
+	 */
 	public Accolade_Node(){
 		this.title=" "
 		this.description=" ";
 		this.check=false;
 	}
+	
+	/**
+	 * Accolades constructor 
+	 * @param title String for title of accolade
+	 * @param description String for description of accolade
+	 * @param check Boolean to represent if user has achieved the accolade 
+	 */
 	public Accolade_Node(String title,String description, boolean check){
 		this.title=title;
 		this.description=description; 
-		this.check=false;
+		this.check=check;
 	}
+	
+	/**
+     * Method returns title of accolade
+     * @return String Title
+     */
 	public String getTitle(){
 		return this.title; 
 	}
-	public boolean getCheck(){
-		return this.check;
-	}
+	
+	/**
+     * Method returns description of accolade
+     * @return String Description
+     */
 	public boolean getDescription(){
 		return this.description;
 	}
+	
+	/**
+     * Method returns check value of accolade
+     * @return Boolean Check
+     */
+	public boolean getCheck(){
+		return this.check;
+	}
+	
+	/**
+     * Method sets check of accolade
+     * @param String title
+     */
 	public void setTitle(String title){
 		this.title=title;
 	}
-	public void setCheck(boolean check){
-		this.check=check;
-	}
+	
+	/**
+     * Method sets description of accolade
+     * @param String description
+     */
 	public void setDescription(String description){
 		this.description=description;
 	}
+	
+	/**
+     * Method sets check value of accolade
+     * @param Boolean check 
+     */
+	public void setCheck(boolean check){
+		this.check=check;
+	}
+	
 }
