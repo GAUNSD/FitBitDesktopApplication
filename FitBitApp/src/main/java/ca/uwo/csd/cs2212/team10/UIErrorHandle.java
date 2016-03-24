309223ef71b9988b0141f866f17dcbc049694bcc
Gustavo Murcia <gmurcia@uwo.ca>
2016-03-24 02:28:06 -0400

    Continued building on the UIError Class

--- FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/UIErrorHandle.java
+++ FitBitApp/src/main/java/ca/uwo/csd/cs2212/team10/UIErrorHandle.java
@@ -19,9 +19,41 @@ import java.awt.event.KeyEvent;
  */
 public class UIErrorHandle extends JPanel {
 	
+	String label;
 	
-	public UIErrorHandle ()	{
+	/**
+	 * UIError constructor.
+	 * This is a JPanel that will display information when there is an error
+	 */
+	public UIErrorHandle() {
 		
+		// Create the label that shows the information
+		this.setLabel("tmp");
+		JTextArea lblName = new JTextArea(this.getLabel());
+		lblName.setEditable(false);
+		lblName.setBackground(new Color(150, 150, 150));
+		lblName.setForeground(new Color(139,24,32));
+		// Set the panels attributes
+		this.add(lblName);
+		this.setBackground(null);
+	}
+	
+	/**
+	 * Mutator to create the String Label that shows the errors.
+	 * 
+	 * @return String
+	 */
+	public void setLabel(String in) {
+		this.label = in;
+	}
+	
+	public String getLabel() {
+		return this.label;
+	}
+	
+	public void updateText() {
+		this.update(this.getGraphics());
+		this.revalidate();
 	}
 	
 	public static void main(String[] args) {
@@ -39,23 +71,17 @@ public class UIErrorHandle extends JPanel {
 		MyMenuBar topMenubar = new MyMenuBar();
 		topMenubar.setBackground(new Color(87, 87, 87));
 		topMenubar.setPreferredSize(new Dimension(1200, 70)); // height
+		
+		// Create the UIErrror Handling Panel.
+		UIErrorHandle uiError = new UIErrorHandle();
+		
 
 		// Create the 'User Name' menu item. Here the user will be able to exit or logout (expand options in the future)
 		JMenu mnuUserName = new JMenu("Window");
 		mnuUserName.setForeground(Color.WHITE);
 		mnuUserName.setBackground(Color.WHITE);
 		mnuUserName.setMnemonic(KeyEvent.VK_F);
-		/*
-		// Under 'User Name': Logout
-		//	this lets the user logout. Takes them to the Sign in page
-		JMenuItem mniUserNameLogout = new JMenuItem("Log Out");
-		mniUserNameLogout.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//Will be linked to the sign in page in the future
-			}
-		});
-		*/
-		//..
+		
 		// Also under 'User Name': Exit
 		//	This lets the user exit the program entirely. (implement the exiting also singing them out and saving settings)
 		JMenuItem mniUserNameExit = new JMenuItem("Exit");
@@ -64,7 +90,8 @@ public class UIErrorHandle extends JPanel {
 		mniUserNameExit.addActionListener(new ActionListener() {
 			//@Override
 			public void actionPerformed(ActionEvent event) {
-				System.exit(0); }
+				uiError.setLabel("Hello");
+				uiError.updateText(); }
 		});
 		
 		// Add the sub-menu items to the dropdown menu
@@ -72,15 +99,19 @@ public class UIErrorHandle extends JPanel {
 		mnuUserName.add(mniUserNameExit);
 
 		// Add all the created elements to their respective containers
-		topMenubar.add(Box.createHorizontalGlue()); // This spaces out the menu item so it's to the left
+		
 		JLabel lblNewLabel = new JLabel("");
 		ImageIcon img0=new ImageIcon("src/main/resources/fitbitlogo.png");
+		topMenubar.add(uiError);
+		//topMenubar.add(Box.createHorizontalGlue()); // This spaces out the menu item so it's to the left
+		topMenubar.add(Box.createRigidArea(new Dimension(20,0))); // This rigidly spaces out the eiError and the space
+		
 		lblNewLabel.setIcon(img0);
 		topMenubar.add(lblNewLabel);
 		topMenubar.add(Box.createHorizontalGlue()); // This spaces out the menu item so it's to the left
 		topMenubar.add(mnuUserName);
 	
-		///////////////////////////
+	
 		// Add it to the main window
 		mainWindow.setJMenuBar(topMenubar);
 		/////////////////////////////////////////////////////////////////////
@@ -89,6 +120,8 @@ public class UIErrorHandle extends JPanel {
 		//MainTabWindow mainTabWindow = new MainTabWindow(fitbit);
 		JPanel mainTabWindow = new JPanel();
 		mainWindow.getContentPane().add(mainTabWindow, BorderLayout.CENTER);
+		
+		mainWindow.setVisible(true);
 		
 	}
 	
